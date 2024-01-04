dd0a366128abcca2c4d84092d1e6d5472b1a7044
rene-huber <96597641+rene-huber@users.noreply.github.com>
2024-01-04 19:24:28 +0100

    login

--- login-demo/index.js
+++ login-demo/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from 'cors';
-import helmet from 'helmet';
+import cors from "cors";
+import helmet from "helmet";
 import path from "path";
+import { fileURLToPath } from 'url';
+
 import dotenv from "dotenv";
 dotenv.config();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 
@@ -14,54 +21,50 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const staff = ["Sally", "Bob", "Mike", "Rachel", "Andy", "Greg"];
-
-
 //**** GET  ****/
 
-app.get("/", (request, response) => {
-  console.log("Someone wants the root route!");
-  response.send("Welcome to our site! 😎");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/staff", (req, res) => {
-  res.json({
-    people: staff,
-  });
-});
-
-app.get("/staff/:person", (req, res) => {
-  const name = req.params.person;
-  if (staff.includes(name)) {
-    res.json({
-      name: name,
-      description: `${name} is a valued employee here at our company!`,
-    });
+app.get("/echo/:message", (req, res) => {
+  const message = req.params.message;
+  if (message === "secret") {
+    res.send("the secret is... 42!");
   } else {
-    res.status(404).json({ error: "Staff member not found" });
+    res.send(message);
   }
 });
 
-app.get("/contact", function (req, res) {
-  console.log("serving contact-form.html...");
-  res.sendFile(path.join(__dirname, "public", "contact-form.html"));
-});
+app.get("/login" , (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+})
+
+app.get("/my-account" , (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "my-account.html"));
+})
+app.get("/error" , (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "error.html"));
+})
+
 
 
 //**** POST  ****/
 
-
-app.post("/contact", function (req, res) {
-  try {
-    console.log("We got a message! Somebody wrote:", req.body.message);
-  res.redirect("/");
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (email === 'admin@example.com' && password === '111') {
+    res.redirect('/my-account');
+  } else {
+    res.redirect('/error');
   }
 });
 
 
+
+
+//****  ****/
+//****  ****/
 
 
 
---
+++ login-demo/public/login.js
@@ -0,0 +1,10 @@
+
+document.querySelector('form').onsubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const response = await fetch('/login', {
+        method: 'POST',
+        body: formData
+    });
+   
+};
